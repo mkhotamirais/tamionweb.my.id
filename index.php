68a460e15ada446d7fc897f131315e2bc787595f
mkhotamirais <mkhotamirais@gmail.com>
2024-11-18 07:31:49 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mkhotami</title>
+    <style>
+        nav {
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Tamionweb</h1>
+    <nav>
+        <a href="/php-basic">php basic</a>
+    </nav>
+</body>
+
+</html>
